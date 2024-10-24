989e34a9ed175f387986966ccdf60e502e90ef9d
Martin Heini <maheini35@me.com>
2024-10-24 23:48:00 +0200

    feat: Add launch server command

--- frappe_betterprint/utils/server.py
+++ frappe_betterprint/utils/server.py
@@ -1,3 +1,44 @@
+import subprocess
+import requests
+import frappe
+import time
+
+
+def launch_server():
+    if not is_betterprint_server_installed():
+        frappe.throw(
+            "Betterprint server seems to be missing on your instance. Please install Betterprint_Server."
+        )
+
+    try:
+        # Launch server by default (won't create 2nd instance)
+        subprocess.Popen(
+            "betterprint_server run",
+            shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        # Repeat if the server isn't ready to accept connections yet
+        for _ in range(3):
+            try:
+                response = requests.get("http://127.0.0.1:39584/v1/status")
+                if response.status_code != 200:
+                    frappe.throw(
+                        f"Couldn't launch betterprint_server: Response invalid. Received statuscode {response.status_code}"
+                    )
+                else:
+                    return
+            except requests.ConnectionError:
+                pass
+
+            time.sleep(0.5)
+
+    except Exception as e:
+        print(e)
+        frappe.throw("ERROR: Couldn't start betterprint_server.")
+
+
 def is_betterprint_server_installed() -> bool:
     """Checks if the server is a callable process on the system"""
     try:
