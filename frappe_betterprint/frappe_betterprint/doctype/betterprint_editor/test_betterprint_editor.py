a406d5bb226283b786d9bbbe9937a4bebaa064d0
Martin Heini <maheini35@me.com>
2024-11-18 23:15:24 +0100

    feat: Add BetterPrint editor single doctype

---
+++ frappe_betterprint/frappe_betterprint/doctype/betterprint_editor/test_betterprint_editor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, neocode.ch and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBetterPrintEditor(FrappeTestCase):
+	pass
