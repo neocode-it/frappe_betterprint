fa4ed1b891047a8104d062d004cc0643c2baf050
Martin Heini <maheini35@me.com>
2024-12-12 23:21:47 +0100

    feat: remove custom doctype

--- frappe_betterprint/frappe_betterprint/doctype/betterprint_editor/betterprint_editor.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, neocode.ch and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class BetterPrintEditor(Document):
-	pass
