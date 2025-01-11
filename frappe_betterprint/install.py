5573616b871b63677ecc40d00b6b545e446f205b
Martin Heini <maheini35@me.com>
2025-01-11 01:03:59 +0100

    feat: Add install-method for custom fields

--- frappe_betterprint/install.py
+++ frappe_betterprint/install.py
@@ -2,24 +2,26 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def after_install():
-    fields = {
-        "Print Format": [
-            {
-                "fieldname": "generate_pdf_by_betterprint",
-                "label": "Generate PDF using Frappe Betterprint",
-                "fieldtype": "Check",
-                "default": "0",
-                "insert_after": "custom_format",
-            },
-            {
-                "fieldname": "betterprint_pdf_page_size",
-                "label": "Betterprint PDF Page Size",
-                "fieldtype": "Select",
-                "options": "A0\nA1\nA2\nA3\nA4\nA5\nA6\nA7\nA8\nA9\nB0\nB1\nB2\nB3\nB4\nB5\nB6\nB7\nB8\nB9\nB10\nC5E\nComm10E\nDLE\nExecutive\nFolio\nLedger\nLegal\nLetter\nTabloid",
-                "depends_on": "generate_pdf_by_betterprint",
-                "default": "A4",
-                "insert_after": "generate_pdf_by_betterprint",
-            },
-        ]
-    }
     create_custom_fields(fields)
+
+
+fields = {
+    "Print Format": [
+        {
+            "fieldname": "generate_pdf_by_betterprint",
+            "label": "Generate PDF using Frappe Betterprint",
+            "fieldtype": "Check",
+            "default": "0",
+            "insert_after": "custom_format",
+        },
+        {
+            "fieldname": "betterprint_pdf_page_size",
+            "label": "Betterprint PDF Page Size",
+            "fieldtype": "Select",
+            "options": "A0\nA1\nA2\nA3\nA4\nA5\nA6\nA7\nA8\nA9\nB0\nB1\nB2\nB3\nB4\nB5\nB6\nB7\nB8\nB9\nB10\nC5E\nComm10E\nDLE\nExecutive\nFolio\nLedger\nLegal\nLetter\nTabloid",
+            "depends_on": "generate_pdf_by_betterprint",
+            "default": "A4",
+            "insert_after": "generate_pdf_by_betterprint",
+        },
+    ]
+}
