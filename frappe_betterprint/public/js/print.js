31ac627966ff4d2026c3d7532133c4a5de13e86f
Martin Heini <maheini35@me.com>
2025-02-07 23:58:12 +0100

    feat: previewlistener

--- frappe_betterprint/public/js/print.js
+++ frappe_betterprint/public/js/print.js
@@ -11,6 +11,55 @@ class BetterPrint{
         // TODO: Prevent/delay print trigger (frappe script) until rendering is done for print-view
     }
 
+    registerPreviewListener(callback) {
+        const targetNode = document.body;
+    
+        const observer = new MutationObserver((mutationsList) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'data-ajax-state') {
+                    callback(targetNode.getAttribute('data-ajax-state'));
+                }
+            }
+        });
+    
+        observer.observe(targetNode, { attributes: true });
+    
+        // Check for the initial state
+        const initialState = targetNode.getAttribute('data-ajax-state');
+        if (initialState !== null) {
+            callback(initialState);
+        }
+    }
+    
+    onPreviewStateChange(newState){
+        if (newState !== "complete"){
+            return;
+        }
+
+        this.iframe = document.querySelector(".print-preview iframe");
+        this.previewDocument = this.iframe?.contentDocument ?? null;
+        this.printPreview = document.querySelector(".print-preview");
+        this.printFormat = this.previewDocument?.querySelector(".print-format") ?? null;
+        
+        if(!this.iframe || !this.previewDocument || !this.printPreview || !this.printFormat){
+            return;
+        }
+
+        this.printContentElement = this.previewDocument.querySelector(".betterprint-content");
+
+        if(!this.printContentElement){
+            // Page doesn't contain betterprint, remove default styles
+            this.resetPreviewStyles();
+            return;
+        } else if(this.printContentElement.classList.contains("rendered")){
+            // Preview has already been rendered... nothing to do here.
+            return;
+        } else {
+            this.printContentElement.classList.add("rendered");
+        }
+        this.renderPreview();
+    }
+
 }
 
 new BetterPrint();
