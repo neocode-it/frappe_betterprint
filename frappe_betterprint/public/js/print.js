6ef4a71f3a7abc3a13b9f545bbf2ef0e2d8eb678
Martin Heini <maheini35@me.com>
2025-03-28 22:39:16 +0100

    style: auto-format js

--- frappe_betterprint/public/js/print.js
+++ frappe_betterprint/public/js/print.js
@@ -1,164 +1,180 @@
-class BetterPrint{
+class BetterPrint {
+  constructor() {
+    if (typeof frappe === "undefined") {
+      document.addEventListener("DOMContentLoaded", this.renderPrint());
+    } else {
+      this.isPreview = true;
+      this.registerPreviewListener(this.onPreviewStateChange.bind(this));
+    }
+  }
 
-    constructor(){
-        if (typeof frappe === "undefined"){
-            document.addEventListener("DOMContentLoaded", this.renderPrint());
-        } else {
-            this.isPreview = true;
-            this.registerPreviewListener(this.onPreviewStateChange.bind(this));
+  registerPreviewListener(callback) {
+    const targetNode = document.body;
+
+    const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (
+          mutation.type === "attributes" &&
+          mutation.attributeName === "data-ajax-state"
+        ) {
+          callback(targetNode.getAttribute("data-ajax-state"));
         }
+      }
+    });
+
+    observer.observe(targetNode, { attributes: true });
+
+    // Check for the initial state
+    const initialState = targetNode.getAttribute("data-ajax-state");
+    if (initialState !== null) {
+      callback(initialState);
+    }
+  }
+
+  onPreviewStateChange(newState) {
+    if (newState !== "complete") {
+      return;
     }
 
-    registerPreviewListener(callback) {
-        const targetNode = document.body;
-    
-        const observer = new MutationObserver((mutationsList) => {
-            for (const mutation of mutationsList) {
-                if (mutation.type === 'attributes' && mutation.attributeName === 'data-ajax-state') {
-                    callback(targetNode.getAttribute('data-ajax-state'));
-                }
-            }
-        });
-    
-        observer.observe(targetNode, { attributes: true });
-    
-        // Check for the initial state
-        const initialState = targetNode.getAttribute('data-ajax-state');
-        if (initialState !== null) {
-            callback(initialState);
-        }
-    }
-    
-    onPreviewStateChange(newState){
-        if (newState !== "complete"){
-            return;
-        }
+    this.iframe = document.querySelector(".print-preview iframe");
+    this.previewDocument = this.iframe?.contentDocument ?? null;
+    this.printPreview = document.querySelector(".print-preview");
+    this.printFormat =
+      this.previewDocument?.querySelector(".print-format") ?? null;
 
-        this.iframe = document.querySelector(".print-preview iframe");
-        this.previewDocument = this.iframe?.contentDocument ?? null;
-        this.printPreview = document.querySelector(".print-preview");
-        this.printFormat = this.previewDocument?.querySelector(".print-format") ?? null;
-        
-        if(!this.iframe || !this.previewDocument || !this.printPreview || !this.printFormat){
-            return;
-        }
-
-        this.printContentElement = this.previewDocument.querySelector(".betterprint-content");
-
-        if(!this.printContentElement){
-            // Page doesn't contain betterprint, remove default styles
-            this.resetPreviewStyles();
-            return;
-        } else if(this.printContentElement.classList.contains("rendered")){
-            // Preview has already been rendered... nothing to do here.
-            return;
-        } else {
-            this.printContentElement.classList.add("rendered");
-        }
-        this.renderPreview();
+    if (
+      !this.iframe ||
+      !this.previewDocument ||
+      !this.printPreview ||
+      !this.printFormat
+    ) {
+      return;
     }
 
-    renderPreview(){
-        // Reset possible previous styles
-        this.resetPreviewStyles(true);
+    this.printContentElement = this.previewDocument.querySelector(
+      ".betterprint-content"
+    );
 
-        // Prepare new styles
-        this.preparePreviewStyles();
+    if (!this.printContentElement) {
+      // Page doesn't contain betterprint, remove default styles
+      this.resetPreviewStyles();
+      return;
+    } else if (this.printContentElement.classList.contains("rendered")) {
+      // Preview has already been rendered... nothing to do here.
+      return;
+    } else {
+      this.printContentElement.classList.add("rendered");
+    }
+    this.renderPreview();
+  }
 
-        // Initialize the Previewer using the global Paged object
-        let paged = new Paged.Previewer(); // PagedModule
+  renderPreview() {
+    // Reset possible previous styles
+    this.resetPreviewStyles(true);
 
-        paged.preview( this.printContentElement.content, null, this.printFormat, this.previewDocument).then(this.afterPreviewRendering.bind(this));
+    // Prepare new styles
+    this.preparePreviewStyles();
+
+    // Initialize the Previewer using the global Paged object
+    let paged = new Paged.Previewer(); // PagedModule
+
+    paged
+      .preview(
+        this.printContentElement.content,
+        null,
+        this.printFormat,
+        this.previewDocument
+      )
+      .then(this.afterPreviewRendering.bind(this));
+  }
+
+  afterPreviewRendering(flow) {
+    console.log("Rendered", flow.total, "pages.");
+
+    const contentWidth = this.printFormat.scrollWidth;
+    let scale = 1;
+
+    // Adjust height of parent to content
+    this.printPreview.style.width = contentWidth + "px";
+    // Check actual rendered width
+    const actualParentWidth = this.printPreview.offsetWidth;
+
+    // Content larger than maxWidth of parent? scale content down
+    if (actualParentWidth < contentWidth) {
+      scale = actualParentWidth / contentWidth;
+      this.iframe.contentDocument.body.style.transform = `scale(${scale})`;
+      // This ensures the content scales from the top-left corner
+      this.iframe.contentDocument.body.style.transformOrigin = "0 0";
     }
 
-    afterPreviewRendering(flow){
-        console.log("Rendered", flow.total, "pages.");
-        
-        const contentWidth = this.printFormat.scrollWidth;
-        let scale = 1;
+    // Adjust height
+    // Needs to have a delay, since frappe's print.js will adjust the height by itself, which might reset height in this case.
+    setTimeout(() => {
+      this.iframe.style.height =
+        Math.round(this.printFormat.scrollHeight * scale) + "px";
 
-        // Adjust height of parent to content
-        this.printPreview.style.width = contentWidth + "px";
-        // Check actual rendered width
-        const actualParentWidth = this.printPreview.offsetWidth;
+      this.emitFinishEvent();
+    }, 500);
+  }
 
-        
+  resetPreviewStyles(keepDimensions = false) {
+    const height = this.iframe.offsetHeight + "px";
+    const width = this.printPreview.offsetWidth + "px";
 
-        // Content larger than maxWidth of parent? scale content down
-        if(actualParentWidth < contentWidth){
-            scale = actualParentWidth / contentWidth;
-            this.iframe.contentDocument.body.style.transform = `scale(${scale})`;
-            // This ensures the content scales from the top-left corner
-            this.iframe.contentDocument.body.style.transformOrigin = "0 0";
-        }
+    this.printPreview.removeAttribute("style");
+    this.printFormat.removeAttribute("style");
+    this.iframe.removeAttribute("style");
+    this.previewDocument.body.removeAttribute("style");
 
-        // Adjust height
-        // Needs to have a delay, since frappe's print.js will adjust the height by itself, which might reset height in this case.
-        setTimeout(() => {
-            this.iframe.style.height = Math.round(this.printFormat.scrollHeight * scale) + "px";
-
-            this.emitFinishEvent();
-        }, 500);
+    if (keepDimensions) {
+      this.printPreview.style.width = width;
+      this.iframe.style.height = height;
     }
+  }
 
-    resetPreviewStyles(keepDimensions = false){    
-        const height = this.iframe.offsetHeight + "px";
-        const width = this.printPreview.offsetWidth + "px";
+  preparePreviewStyles() {
+    // Modify print-format styles in order to prevent issues between pagedjs and default frappe rendering
+    this.printFormat.style.padding = "0";
+    this.printFormat.style.margin = "0";
+    this.printFormat.style.width = "fit-content";
+    this.printPreview.style.minHeight = "unset";
+  }
 
-        this.printPreview.removeAttribute("style");
-        this.printFormat.removeAttribute("style");
-        this.iframe.removeAttribute("style");
-        this.previewDocument.body.removeAttribute("style");
+  async renderPrint() {
+    this.printFormat = document.querySelector(".print-format");
 
-        if(keepDimensions){
-            this.printPreview.style.width = width;
-            this.iframe.style.height = height;
-        }
-    }
+    this.preparePrintStyles();
 
-    preparePreviewStyles(){
-        // Modify print-format styles in order to prevent issues between pagedjs and default frappe rendering
-        this.printFormat.style.padding = "0";
-        this.printFormat.style.margin = "0";
-        this.printFormat.style.width = "fit-content";
-        this.printPreview.style.minHeight = "unset";
-    }
+    const dom = this.printFormat.querySelector(".betterprint-content");
 
-    async renderPrint(){
-        this.printFormat = document.querySelector(".print-format");
+    if (!dom) return;
 
-        this.preparePrintStyles();
+    let paged = new Paged.Previewer();
 
-        const dom = this.printFormat.querySelector(".betterprint-content");
+    paged
+      .preview(dom.content, null, this.printFormat)
+      .then(this.afterPrintRendering.bind(this));
+  }
 
-        if(!dom) return;
+  preparePrintStyles() {
+    this.printFormat.style.minHeight = "unset";
+    this.printFormat.style.margin = "0";
+    this.printFormat.style.padding = "0";
 
-        let paged = new Paged.Previewer();
-        
-        
-        paged.preview( dom.content, null, this.printFormat ).then(this.afterPrintRendering.bind(this));
-    }
+    document.querySelector(".print-format-gutter").style.minHeight = "90vh";
+  }
 
-    preparePrintStyles(){
-        this.printFormat.style.minHeight = "unset";
-        this.printFormat.style.margin = "0";
-        this.printFormat.style.padding = "0";
+  afterPrintRendering(flow) {
+    console.log("Rendered", flow.total, "pages.");
 
-        document.querySelector(".print-format-gutter").style.minHeight = "90vh";
-    }
+    document.querySelector(".action-banner").classList.remove("print-hide");
 
-    afterPrintRendering(flow){
-        console.log("Rendered", flow.total, "pages.");
+    document.addEventListener("DOMContentLoaded", function () {
+      console.log("again");
+    });
 
-        document.querySelector(".action-banner").classList.remove("print-hide");
-
-        document.addEventListener("DOMContentLoaded", function() {
-            console.log("again");
-        });
-        
-        
-        let style = document.createElement('style');
-        style.textContent = `
+    let style = document.createElement("style");
+    style.textContent = `
             .print-format-gutter{
             justify-content: center;
             display: flex;
@@ -191,36 +207,37 @@ class BetterPrint{
             }
         `;
 
-        document.head.appendChild(style);
-            
-        this.emitFinishEvent();
+    document.head.appendChild(style);
 
-        this.checkAndTriggerPrint();
+    this.emitFinishEvent();
+
+    this.checkAndTriggerPrint();
+  }
+
+  checkAndTriggerPrint() {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Check if the 'trigger_print' parameter is set to '1'
+    if (urlParams.get("trigger_print") === "1") {
+      window.print();
+      // close the window after print
+      // NOTE: doesn't close if print is cancelled in Chrome
+      // Changed timeout to 5s from 1s because it blocked mobile view rendering
+      setTimeout(function () {
+        window.close();
+      }, 5000);
     }
+  }
 
-    checkAndTriggerPrint(){
-        const urlParams = new URLSearchParams(window.location.search);
-
-        // Check if the 'trigger_print' parameter is set to '1'
-        if (urlParams.get('trigger_print') === '1') {
-            window.print();
-            // close the window after print
-            // NOTE: doesn't close if print is cancelled in Chrome
-            // Changed timeout to 5s from 1s because it blocked mobile view rendering
-            setTimeout(function() {
-                window.close();
-            }, 5000);
-        }
-    }
-
-    emitFinishEvent(){
-        document.dispatchEvent(new CustomEvent("betterPrintFinished", {
-            detail: { 
-                message: "Betterprint has finished rendering page!"
-            }
-        }));
-    }
-
+  emitFinishEvent() {
+    document.dispatchEvent(
+      new CustomEvent("betterPrintFinished", {
+        detail: {
+          message: "Betterprint has finished rendering page!",
+        },
+      })
+    );
+  }
 }
 
 new BetterPrint();
