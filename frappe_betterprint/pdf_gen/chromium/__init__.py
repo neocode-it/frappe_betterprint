93f8df52093fbe519ff5fc993ef0436f2f07a4b1
maheini <maheini35@me.com>
2025-05-07 17:20:55 +0200

    fix: change chromium exec path

--- frappe_betterprint/pdf_gen/chromium/__init__.py
+++ frappe_betterprint/pdf_gen/chromium/__init__.py
@@ -6,7 +6,7 @@ import os
 def generate_pdf(html, site_url):
     "Generate a PDF from HTML content using Chromium."
     bench = frappe.utils.get_bench_path()
-    chrome_path = os.path.join(bench, "betterprint-chrome")
+    chrome_path = os.path.join(bench, "betterprint-chrome", "chrome")
     with Browser(chrome_path) as browser:
         page = browser.new_page(mock_domain=site_url)
 
