ed02716407c8d8a997a4223e4c3350d53e4b8d63
maheini <maheini35@me.com>
2025-05-24 22:31:39 +0200

    feat: Add betterprint_server state check

--- frappe_betterprint/pdf_renderer/__init__.py
+++ frappe_betterprint/pdf_renderer/__init__.py
@@ -20,3 +20,26 @@ def start_server():
         frappe.throw("ERROR: Couldn't start betterprint_server.")
 
 
+def check_server_status(timeout=10):
+    try:
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                response = requests.get("http://127.0.0.1:39584/v1/status", timeout=1)
+                if response.status_code != 200:
+                    frappe.throw(
+                        f"Couldn't launch betterprint_server: Response invalid. Received statuscode {response.status_code}"
+                    )
+                else:
+                    return
+            except requests.ConnectionError:
+                # Server is inaccessible
+                # If the server is not running yet, wait and try again
+                pass
+
+            time.sleep(0.5)
+
+    except Exception as e:
+        frappe.throw(f"Couldn't launch betterprint_server: {e}.")
+
+    return False
