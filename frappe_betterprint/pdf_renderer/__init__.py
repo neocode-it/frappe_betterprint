b8bfd080beaf261735bd3e1c14020261015931d5
maheini <maheini35@me.com>
2025-05-24 22:30:52 +0200

    feat: add function to launch server as a subprocess

--- frappe_betterprint/pdf_renderer/__init__.py
+++ frappe_betterprint/pdf_renderer/__init__.py
@@ -1,24 +1,22 @@
+import subprocess
+import requests
+import sys
+
 import frappe
-import multiprocessing
 
-import frappe_betterprint.pdf_renderer.server as renderer_server
+def start_server():
+    # Maybe add check if the dependencies are installed here...
+
+    try:
+        # Launch server
+        # The server will exit by itself if there's already an instance running
+        subprocess.Popen(
+            [sys.executable, "-m", "frappe_betterprint.pdf_renderer.server"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    except Exception as e:
+        print(e)
+        frappe.throw("ERROR: Couldn't start betterprint_server.")
 
 
-def launch_browser():
-    config = frappe.get_common_site_config()
-
-    external_betterprint = config.get("external_betterprint", None)
-
-    if external_betterprint:
-        raise Exception("This feature is not available in external betterprint yet.")
-
-    server = multiprocessing.Process(target=renderer_server.start_server)
-    server.start()
-
-
-# def get_server_hostname():
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     s.connect(("8.8.8.8", 80))  # Connect to an external server (Google DNS)
-#     local_ip = s.getsockname()[0]
-#     s.close()
-#     return local_ip
