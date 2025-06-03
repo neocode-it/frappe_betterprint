ed7c977502b3e5e88215c9cd1bbbec804b2695f2
maheini <maheini35@me.com>
2025-06-04 01:32:47 +0200

    feat: implement WorkerThread for chromium worker

--- frappe_betterprint/pdf_renderer/worker.py
+++ frappe_betterprint/pdf_renderer/worker.py
@@ -7,9 +7,16 @@ from urllib.parse import urlparse
 from playwright.sync_api import sync_playwright
 
 
-def launch_worker_thread(global_queue):
-    threading.Thread(target=worker_thread, daemon=True, args=(global_queue,)).start()
+class WorkerThread(threading.Thread):
+    def __init__(self, queue):
+        super().__init__()
+        self.queue = queue
+        self.daemon = True
+        self.stop_event = threading.Event()
 
+    def stop(self):
+        """Public method to stop the thread."""
+        self.stop_event.set()  # Signal the thread to stop
 
 def worker_thread(q):
     # Nested try-catch to try-close playwright after every attempt.
