35de9e4830f06df61c71128805016a624263d920
maheini <maheini35@me.com>
2025-06-04 01:35:50 +0200

    feat: implement core worker loop & tasker

--- frappe_betterprint/pdf_renderer/worker.py
+++ frappe_betterprint/pdf_renderer/worker.py
@@ -18,9 +18,50 @@ class WorkerThread(threading.Thread):
         """Public method to stop the thread."""
         self.stop_event.set()  # Signal the thread to stop
 
-def worker_thread(q):
-    # Nested try-catch to try-close playwright after every attempt.
-    # If that fails too, theres a backup try-catch
-    playwright = None
-    browser = None
-    task = None
+    def run(self):
+        # Nested try-catch to try-close playwright after every attempt.
+        # If that fails too, theres a backup try-catch
+        playwright = None
+        browser = None
+        task = None
+
+        try:
+            playwright = sync_playwright().start()
+            browser = playwright.chromium.launch()
+            while not self.stop_event.is_set():
+                try:
+                    task = self.queue.get_task()
+
+                    if task["command"] == "generate-betterprint-pdf":
+                        result = self.generate_betterprint_pdf(task, browser)
+
+                    else:
+                        result = {"error": True, "description": "WORKER ERROR: Command not found"}
+
+                    if result is None:
+                        result = {"error": True, "content": "Missing response from worker"}
+
+                    result = {"error": False, "content": "", **result}
+                    self.queue.task_done(task, result)
+
+                except queue.Empty:
+                    time.sleep(0.2)
+
+        except Exception as _:
+            print("Exception occurred in Workerthread. Gathering log data and restart worker...")
+
+            # Check if there was an interrupt within an ongoing task.
+            # If so, terminate task and report error
+            if task:  # Ongoing task?
+                self.queue.task_done(task, {"error": True, "content": "Error ocurred in workerthread"})
+
+            # Throw exception to signal an issue for the worker_backbone
+            raise Exception("Important! Required to signal worker_backbone there went something wrong")
+        finally:
+            # Try to close browser and playwright if they are initialized.
+            # Might fail too, depending on the exception.
+            if browser:
+                browser.close()
+            if playwright:
+                playwright.stop()
+
