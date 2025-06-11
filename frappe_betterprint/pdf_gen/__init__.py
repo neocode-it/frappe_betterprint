b4b54818e4db7f84b74f735801a6b0bcbaec84f8
maheini <maheini35@me.com>
2025-06-11 23:30:50 +0200

    feat: implement betterprint server - finally :)

--- frappe_betterprint/pdf_gen/__init__.py
+++ frappe_betterprint/pdf_gen/__init__.py
@@ -1,39 +1,51 @@
 import os
 import io
+import re
+import json
+
+import requests
 from pypdf import PdfReader, PdfWriter
 
 import frappe
 from frappe.utils import get_url
-
 import frappe_betterprint.pdf_gen.utils as pdf_gen_utils
+from frappe_betterprint.pdf_renderer import start_server, check_server_status
 
 
 def get_betterprint_pdf(html, options=None, output: PdfWriter | None = None):
     """Will generate betterprint pdf file using chrome"""
-    if not options:
-        options = {}
-
-    # TODO: Implement page size if set in options
-    # page_size = pdf_gen_utils.prepare_page_size(options)
-
-    # pdf_file_path = os.path.abspath(f"/tmp/{frappe.generate_hash()}.pdf")
+    pdf_file_path = os.path.abspath(f"/tmp/{frappe.generate_hash()}.pdf")
+    start_server()
 
     html = pdf_gen_utils.prepare_html_for_external_use(html)
 
-    from frappe_betterprint.pdf_gen.chromium import generate_pdf, log
+    if not check_server_status():
+        frappe.throw(
+            "Betterprint could not be started. Please check if all required dependencies are installed."
+        )
 
-    # pdf_content = generate_pdf(html, get_url())
-    # log("PDF generated, content received")
+    body = {
+        "html": html,
+        "filepath": pdf_file_path,
+        "allow_origin": get_url(),
+    }
 
-    # render_pdf(html, pdf_file_path, get_url())
+    body = json.dumps(body)
 
-    # pdf_content = None
+    response = requests.get(
+        "http://127.0.0.1:39584/v1/generate-betterprint-pdf",
+        data=body,
+        headers={"content-type": "application/json"},
+        timeout=15,
+    )
+    if response.status_code != 200:
+        frappe.throw("PDF generation failed. Invalid status code from betterprint_server")
 
-    # with open(pdf_file_path, "rb") as f:
-    #     pdf_content = f.read()
-    #     os.remove(pdf_file_path)
+    pdf_content = None
 
-    pdf_content = get_betterprint_server_pdf(html, get_url())
+    with open(pdf_file_path, "rb") as f:
+        pdf_content = f.read()
+        os.remove(pdf_file_path)
 
     reader = PdfReader(io.BytesIO(pdf_content))
 
@@ -49,47 +61,4 @@ def get_betterprint_pdf(html, options=None, output: PdfWriter | None = None):
         writer.encrypt(password)
 
     filedata = pdf_gen_utils.get_file_data_from_writer(writer)
-
-    log("Return file content")
-    print("PDF generated, content received")
     return filedata
-
-
-def get_betterprint_server_pdf(html, domain):
-    """
-    Will generate betterprint pdf file using betterprint server
-    Returns the pdf content
-    """
-
-    import json
-    import requests
-
-    filename = frappe.generate_hash() + ".pdf"
-
-    print_path = "/home/" + filename
-    file_path = "/home/frappe/frappe-bench/sites/" + filename
-
-    body = {
-        "html": html,
-        "filepath": print_path,
-        "allow_origin": domain,
-    }
-
-    body = json.dumps(body)
-
-    response = requests.get(
-        "http://debian:39584/v1/generate-betterprint-pdf",
-        data=body,
-        headers={"content-type": "application/json"},
-        timeout=15,
-    )
-    if response.status_code != 200:
-        frappe.throw("PDF generation failed. Invalid status code from betterprint_server")
-
-    pdf_content = None
-
-    with open(file_path, "rb") as f:
-        pdf_content = f.read()
-        os.remove(file_path)
-
-    return pdf_content
