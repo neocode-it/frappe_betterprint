8404bfe12d365e812b0774f240286851f776eed2
maheini <maheini35@me.com>
2025-06-19 01:11:03 +0200

    fix: replace overflow scroll with hidden

--- frappe_betterprint/public/js/paginate.js
+++ frappe_betterprint/public/js/paginate.js
@@ -849,7 +849,7 @@
               content.style.width = "100%";
               content.style.height = "unset";
               content.style.flexGrow = "1";
-              content.style.overflow = "scroll";
+              content.style.overflowY = "hidden";
               const footer = document.createElement("div");
               footer.classList.add("footer");
               footer.style.margin = "0";
